8ce39c55fb92d551e16db947d0b14c1ebc63bfb2
zack-mf <limf1670@163.com>
2021-06-03 15:22:31 +0800

    将结果输出到指定文件

--- README.md
+++ README.md
@@ -8,5 +8,5 @@ gcc test_matrix.c -o test_matrix
 
 # 运行
 ```  
-./test_matrix ./matrix.txt
+./test_matrix ./matrix_sample.txt output.txt
 ```  
--- test_matrix.c
+++ test_matrix.c
@@ -1,22 +1,12 @@
 #include "stdio.h"
 #include <stdlib.h>
 #include <string.h>
-#define MAX_LINE 1024
+#define BUF_MAX 1024
 
 #define MAP(x) (x==0)?x:(10-x)
 #define N 9
 
-int matrix_arr[N][N]={
-                                {1, 2, 3, 4, 5, 6, 7, 8, 9},
-                                {1, 2, 3, 4, 5, 6, 7, 8, 9},
-                                {1, 2, 3, 4, 5, 6, 7, 8, 9},
-                                {1, 2, 3, 4, 5, 6, 7, 8, 9},
-                                {1, 2, 3, 4, 5, 6, 7, 8, 9},
-                                {1, 2, 3, 4, 5, 6, 7, 8, 9},
-                                {1, 2, 3, 4, 5, 6, 7, 8, 9},
-                                {1, 2, 3, 4, 5, 6, 7, 8, 9},
-                                {1, 2, 3, 4, 5, 6, 7, 8, 9}
-                            };
+int matrix_arr[N][N]={0};
 
 void get_number(char *buf, const char *slipt_s, int *array, int size){
 	char *slipt_buf[N], *p;
@@ -37,52 +27,80 @@ void get_number(char *buf, const char *slipt_s, int *array, int size){
 }
 
 void main(int argc,char *argv[]){
-	char buf[MAX_LINE];		/*缓冲区*/
-	FILE *fp; 				/*文件指针*/
+	char buf[BUF_MAX];		/*缓冲区*/
+	FILE *input_fp; 		/*文件指针*/
+	FILE *output_fp;
  	int len;				/*行字符个数*/
 	int line=0;	     		/*文本的行数*/
+	char int_str[16]={0};
 
-	if(argc < 2){
-		printf("please input the matrix.txt path\n");
+	if(argc < 3){
+		printf("%s input_file output_file\n", argv[0]);
 		exit(1);
 	}
 
-	if((fp = fopen(argv[1],"r")) == NULL)
+	if((input_fp = fopen(argv[1],"r")) == NULL)
 	{
-		printf("fail to read %s\n", argv[1]);
+		printf("fail to open %s\n", argv[1]);
 		exit(1);
 	}
-	while(fgets(buf,MAX_LINE,fp) != NULL)
+
+	if((output_fp = fopen(argv[2],"w")) == NULL)
+    {
+        printf("fail to open %s\n", argv[2]);
+        exit(1);
+    }
+
+	fwrite("source array:\n", strlen("source array:\n"), 1, output_fp);
+	while(fgets(buf,BUF_MAX,input_fp) != NULL)
 	{
 		len = strlen(buf);
 		//printf("%s\n", buf);
-		if(line < N)
+		if(line < N){
+			fwrite(buf, len, 1, output_fp);
 			get_number(buf, ",", matrix_arr[line++], N);
+			memset(buf, '\0', BUF_MAX);
+		}
 	}
+	fwrite("\n", strlen("\n"), 1, output_fp);
+	fclose(input_fp);
 
-	printf("source array:\n");
+    fwrite("ts array:\n", strlen("ts array:\n"), 1, output_fp);
 	for(int i=0; i<N; i++){
 		for(int j=0; j<N; j++){
-			printf("%d ", matrix_arr[i][j]);	
+			if(matrix_arr[j][i] != 0){
+				sprintf(int_str, "%d,", matrix_arr[j][i]);
+            }else{
+				sprintf(int_str, " ,");
+            }
+            if(j == N-1){
+				fwrite(int_str, strlen(int_str)-1, 1, output_fp);
+			}else{
+				fwrite(int_str, strlen(int_str), 1, output_fp);
+			}
+			memset(int_str, '\0', (int)( sizeof(int_str) / sizeof(int_str[0]) ));
 		}
-		printf("\n");
+		fwrite("\n", strlen("\n"), 1, output_fp);
 	}
 
-	printf("ts array:\n");
+	fwrite("from ts array calculate map array:\n", strlen("from ts array calculate map array:\n"), 1, output_fp);
 	for(int i=0; i<N; i++){
 		for(int j=0; j<N; j++){
-			printf("%d ", matrix_arr[j][i]);
-		}
-		printf("\n");
-	}
-
-	printf("from ts array calculate map array:\n");
-	for(int i=0; i<N; i++){
-                for(int j=0; j<N; j++){
-                        printf("%d ", MAP(matrix_arr[j][i]));
-
-                }
-                printf("\n");
+			if(matrix_arr[j][i] != 0){
+				sprintf(int_str, "%d,", MAP(matrix_arr[j][i]));
+			}else{
+				sprintf(int_str, " ,");
+			}
+			if(j == N-1){
+				fwrite(int_str, strlen(int_str)-1, 1, output_fp);
+			}else{
+				fwrite(int_str, strlen(int_str), 1, output_fp);
+			}
+				memset(int_str, '\0', (int)( sizeof(int_str) / sizeof(int_str[0]) ));
+			}
+			fwrite("\n", strlen("\n"), 1, output_fp);
         }
+
+	fclose(output_fp);
 	
 }
